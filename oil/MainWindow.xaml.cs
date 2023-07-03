edaf556e74bc8eb7d1240aae2a71263e4346370c
NazwayDen20 <d.malinovskyi99@gmail.com>
2023-07-03 23:05:50 +0300

    denibob krasava

--- oil/MainWindow.xaml.cs
+++ oil/MainWindow.xaml.cs
@@ -18,11 +18,73 @@ namespace oil
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Person
+    {
+        public string Name { get; set; } = "";
+
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
     public partial class MainWindow : Window
     {
+        public Person selectedPerson;
+        List<Person> people = new List<Person>()
+        {
+             new Person{Name="Tom" },
+            new Person {Name="Bob"},
+            new Person{Name="Sam"}
+        };
+        List<Person> whiteList = new List<Person>()
+        {
+    
+        };
+        List<Person> blackList = new List<Person>()
+        {
+
+        };
         public MainWindow()
         {
             InitializeComponent();
+            AllList.ItemsSource = people;
+            BlackList.ItemsSource = blackList;
+            WhiteList.ItemsSource = whiteList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            whiteList.Add(selectedPerson);
+            WhiteList.Items.Refresh();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            whiteList.Clear();
+            WhiteList.Items.Refresh();
+        }
+
+        private void WhiteText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            blackList.Add(selectedPerson);
+            BlackList.Items.Refresh();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            blackList.Clear();
+            BlackList.Items.Refresh();
+        }
+        private void AllList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedPerson = AllList.SelectedItem as Person;
         }
     }
 }
