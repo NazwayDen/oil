c3c8223465eb753d06bb8e68861f03530c187994
NazwayDen20 <d.malinovskyi99@gmail.com>
2023-07-04 21:25:12 +0300

    db

---
+++ oil/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace oil
+{
+    class ApplicationContext:DbContext
+    {
+        public DbSet<OIl> Oils { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=oilDB.db");
+        }
+    }
+}
